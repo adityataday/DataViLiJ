9feabb9802d7da3c2a270ba34d244b1215b83c2a
Aditya Taday <aditya.taday@stonybrook.edu>
2018-02-06 18:51:43 -0500

    ScreenShot Button added

--- hw1/data-vilij/src/ui/AppUI.java
+++ hw1/data-vilij/src/ui/AppUI.java
@@ -1,10 +1,17 @@
 package ui;
 
 import actions.AppActions;
+import static java.io.File.separator;
 import javafx.scene.chart.ScatterChart;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.ToolBar;
 import javafx.stage.Stage;
+import static settings.AppPropertyTypes.SCREENSHOT_ICON;
+import static settings.AppPropertyTypes.SCREENSHOT_TOOLTIP;
+import vilij.propertymanager.PropertyManager;
+import static vilij.settings.PropertyTypes.GUI_RESOURCE_PATH;
+import static vilij.settings.PropertyTypes.ICONS_RESOURCE_PATH;
 import vilij.templates.ApplicationTemplate;
 import vilij.templates.UITemplate;
 
@@ -15,17 +22,23 @@ import vilij.templates.UITemplate;
  */
 public final class AppUI extends UITemplate {
 
-    /** The application to which this class of actions belongs. */
+    /**
+     * The application to which this class of actions belongs.
+     */
     ApplicationTemplate applicationTemplate;
 
     @SuppressWarnings("FieldCanBeLocal")
-    private Button                       scrnshotButton; // toolbar button to take a screenshot of the data
+    private Button scrnshotButton; // toolbar button to take a screenshot of the data
     private ScatterChart<Number, Number> chart;          // the chart where data will be displayed
-    private Button                       displayButton;  // workspace button to display data on the chart
-    private TextArea                     textArea;       // text area for new data input
-    private boolean                      hasNewText;     // whether or not the text area has any new data since last display
+    private Button displayButton;  // workspace button to display data on the chart
+    private TextArea textArea;       // text area for new data input
+    private boolean hasNewText;     // whether or not the text area has any new data since last display
 
-    public ScatterChart<Number, Number> getChart() { return chart; }
+    private String scrnshoticonPath;
+
+    public ScatterChart<Number, Number> getChart() {
+        return chart;
+    }
 
     public AppUI(Stage primaryStage, ApplicationTemplate applicationTemplate) {
         super(primaryStage, applicationTemplate);
@@ -35,17 +48,26 @@ public final class AppUI extends UITemplate {
     @Override
     protected void setResourcePaths(ApplicationTemplate applicationTemplate) {
         super.setResourcePaths(applicationTemplate);
+        PropertyManager manager = applicationTemplate.manager;
+        String iconsPath = "/" + String.join(separator,
+                                             manager.getPropertyValue(GUI_RESOURCE_PATH.name()),
+                                             manager.getPropertyValue(ICONS_RESOURCE_PATH.name()));
+
+        scrnshoticonPath = String.join(separator, iconsPath, manager.getPropertyValue(SCREENSHOT_ICON.name()));
     }
 
     @Override
     protected void setToolBar(ApplicationTemplate applicationTemplate) {
         // Calling this method from the super class.
-        super.setToolBar(applicationTemplate); 
+        super.setToolBar(applicationTemplate);
+        PropertyManager manager = applicationTemplate.manager;
+        scrnshotButton = setToolbarButton(scrnshoticonPath, manager.getPropertyValue(SCREENSHOT_TOOLTIP.name()), false);
+        toolBar = new ToolBar(newButton, saveButton, loadButton, printButton, exitButton, scrnshotButton);
     }
 
     @Override
     protected void setToolbarHandlers(ApplicationTemplate applicationTemplate) {
-        applicationTemplate.setActionComponent(new AppActions(applicationTemplate));    
+        applicationTemplate.setActionComponent(new AppActions(applicationTemplate));
         newButton.setOnAction(e -> applicationTemplate.getActionComponent().handleNewRequest());
         saveButton.setOnAction(e -> applicationTemplate.getActionComponent().handleSaveRequest());
         loadButton.setOnAction(e -> applicationTemplate.getActionComponent().handleLoadRequest());
@@ -66,9 +88,11 @@ public final class AppUI extends UITemplate {
 
     private void layout() {
         // TODO for homework 1
+        getChart();
     }
 
     private void setWorkspaceActions() {
         // TODO for homework 1
+
     }
 }
