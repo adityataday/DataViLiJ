9cbd4dc7e241123a97203efed62acd650747e7d8
Aditya Taday <adi.taday@gmail.com>
2018-04-12 19:14:34 -0400

    H4 initialized

---
+++ hw4/data-vilij/src/dataprocessors/TSDProcessor.java
@@ -0,0 +1,212 @@
+package dataprocessors;
+
+import javafx.geometry.Point2D;
+import javafx.scene.chart.XYChart;
+
+import java.util.*;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Stream;
+import javafx.scene.control.Tooltip;
+import static settings.AppPropertyTypes.LABEL_ALREADY_EXISTS;
+import static settings.AppPropertyTypes.TO_MANY_LINES;
+import static settings.AppPropertyTypes.TO_MANY_LINES_MSG_1;
+import static settings.AppPropertyTypes.TO_MANY_LINES_MSG_2;
+import vilij.components.Dialog;
+import vilij.components.ErrorDialog;
+import vilij.propertymanager.PropertyManager;
+import vilij.templates.ApplicationTemplate;
+
+/**
+ * The data files used by this data visualization applications follow a
+ * tab-separated format, where each data point is named, labeled, and has a
+ * specific location in the 2-dimensional X-Y plane. This class handles the
+ * parsing and processing of such data. It also handles exporting the data to a
+ * 2-D plot.
+ * <p>
+ * A sample file in this format has been provided in the application's
+ * <code>resources/data</code> folder.
+ *
+ * @author Ritwik Banerjee
+ * @see XYChart
+ */
+public final class TSDProcessor {
+
+    public static class InvalidDataNameException extends Exception {
+
+        private static final String NAME_ERROR_MSG = "All data instance names must start with the @ character.";
+
+        public InvalidDataNameException(String name) {
+            super(String.format("Invalid name '%s'." + NAME_ERROR_MSG, name));
+        }
+    }
+
+    private static final String ERROR_LINE_ID = "Error in line : ";
+    private static final String CORRECT_DATA_FORMAT = "Correct Input must be [@Instance \\t label \\t data]";
+
+    private Map<String, String> dataLabels;
+    private Map<String, Point2D> dataPoints;
+    private ApplicationTemplate applicationTemplate;
+
+    public TSDProcessor(ApplicationTemplate applicationTemplate) {
+        dataLabels = new LinkedHashMap<>();
+        dataPoints = new HashMap<>();
+        this.applicationTemplate = applicationTemplate;
+    }
+
+    /**
+     * Processes the data and populated two {@link Map} objects with the data.
+     *
+     * @param tsdString the input data provided as a single {@link String}
+     * @throws Exception if the input string does not follow the
+     * <code>.tsd</code> data format
+     */
+    public void processString(String tsdString) throws Exception {
+        AtomicBoolean hadAnError = new AtomicBoolean(false);
+        StringBuilder errorMessage = new StringBuilder();
+        AtomicInteger count = new AtomicInteger();
+        Stream.of(tsdString.split("\n"))
+                .map(line -> Arrays.asList(line.split("\t")))
+                .forEach(list -> {
+                    try {
+                        count.incrementAndGet();
+                        String name = checkedname(list.get(0));
+                        String label = list.get(1);
+                        String[] pair = list.get(2).split(",");
+                        Point2D point = new Point2D(Double.parseDouble(pair[0]), Double.parseDouble(pair[1]));
+                        checkInstanceDuplicates(name);
+                        dataLabels.put(name, label);
+                        dataPoints.put(name, point);
+                    } catch (Exception e) {
+                        //errorMessage.setLength(0);
+                        if (e instanceof ArrayIndexOutOfBoundsException) {
+                            errorMessage.append("\n").append(ERROR_LINE_ID + count + " " + list.toString() + " ").append(CORRECT_DATA_FORMAT);
+                        } else {
+                            errorMessage.append("\n").append(ERROR_LINE_ID + count + " " + list.toString() + " ").append(e.getMessage());
+                        }
+                        hadAnError.set(true);
+                    }
+                });
+        if (errorMessage.length() > 0) {
+            throw new Exception(errorMessage.toString());
+        }
+    }
+
+    /**
+     * Exports the data to the specified 2-D chart.
+     *
+     * @param chart the specified chart
+     */
+    void toChartData(XYChart<Number, Number> chart) {
+        Set<String> labels;
+
+        if (dataLabels.size() <= 10) {
+            labels = new HashSet<>(dataLabels.values());
+        } else {
+            displayFilter();
+            labels = new HashSet<>(dataLabels.values());
+        }
+
+        for (String label : labels) {
+            XYChart.Series<Number, Number> series = new XYChart.Series<>();
+            series.setName(label);
+            dataLabels.entrySet().stream().filter(entry -> entry.getValue().equals(label)).forEach(entry -> {
+                Point2D point = dataPoints.get(entry.getKey());
+                series.getData().add(new XYChart.Data<>(point.getX(), point.getY()));
+            });
+            chart.getData().add(series);
+        }
+
+        averageY(chart);
+
+    }
+
+    void clear() {
+        dataPoints.clear();
+        dataLabels.clear();
+    }
+
+    private String checkedname(String name) throws InvalidDataNameException {
+        if (!name.startsWith("@")) {
+            throw new InvalidDataNameException(name);
+        }
+
+        return name;
+    }
+
+    private void checkInstanceDuplicates(String name) throws Exception {
+        if (dataLabels.containsKey(name)) {
+            throw new Exception(name + applicationTemplate.manager.getPropertyValue(LABEL_ALREADY_EXISTS.name()));
+        }
+
+    }
+
+    private void displayFilter() {
+        ArrayList<String> keys = new ArrayList<>(dataLabels.keySet());
+
+        for (int i = 0; i < keys.size(); i++) {
+            if (i >= 10) {
+                dataLabels.remove(keys.get(i));
+            }
+        }
+
+        errorHandlingHelper(keys.size());
+
+    }
+
+    private void errorHandlingHelper(int size) {
+        ErrorDialog dialog = (ErrorDialog) applicationTemplate.getDialog(Dialog.DialogType.ERROR);
+        PropertyManager manager = applicationTemplate.manager;
+        String errTitle = manager.getPropertyValue(TO_MANY_LINES.name());
+        String errMsg = manager.getPropertyValue(TO_MANY_LINES_MSG_1.name()) + size + manager.getPropertyValue(TO_MANY_LINES_MSG_2.name());
+        dialog.show(errTitle, errMsg);
+    }
+
+    private void averageY(XYChart<Number, Number> chart) {
+        double sum = 0;
+        int count = 0;
+        double minX = Integer.MAX_VALUE;
+        double maxX = Integer.MIN_VALUE;
+        for (int i = 0; i < chart.getData().size(); i++) {
+            for (int j = 0; j < chart.getData().get(i).getData().size(); j++) {
+
+                if (minX > (Double) chart.getData().get(i).getData().get(j).getXValue()) {
+                    minX = (Double) chart.getData().get(i).getData().get(j).getXValue();
+                }
+
+                if (maxX < (Double) chart.getData().get(i).getData().get(j).getXValue()) {
+                    maxX = (Double) chart.getData().get(i).getData().get(j).getXValue();
+                }
+
+                sum += (Double) chart.getData().get(i).getData().get(j).getYValue();
+                count++;
+            }
+
+        }
+
+        XYChart.Series<Number, Number> averageY = new XYChart.Series<>();
+        averageY.getData().add(new XYChart.Data<>(minX, (sum / count)));
+        averageY.getData().add(new XYChart.Data<>(maxX, (sum / count)));
+        averageY.setName("Average Y Value");
+
+        chart.getData().add(averageY);
+
+        for (int i = 0; i < chart.getData().size(); i++) {
+            if (i == chart.getData().size() - 1) {
+                break;
+            }
+
+            chart.getData().get(i).getNode().setVisible(false);
+
+        }
+
+        for (int i = 0; i < chart.getData().size(); i++) {
+            for (int j = 0; j < chart.getData().get(i).getData().size(); j++) {
+                Tooltip.install(chart.getData().get(i).getData().get(j).getNode(), new Tooltip(chart.getData().get(i).getData().get(j).getXValue().toString()+", " +chart.getData().get(i).getData().get(j).getYValue().toString()));
+               
+            }
+
+        }
+
+    }
+}
---
+++ hw4/data-vilij/src/ui/AppUI.java
@@ -0,0 +1,235 @@
+package ui;
+
+import actions.AppActions;
+import dataprocessors.AppData;
+import static java.io.File.separator;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import settings.AppPropertyTypes;
+import vilij.propertymanager.PropertyManager;
+import vilij.templates.ApplicationTemplate;
+import vilij.templates.UITemplate;
+
+import javafx.scene.control.CheckBox;
+import static vilij.settings.PropertyTypes.CSS_RESOURCE_FILENAME;
+import static vilij.settings.PropertyTypes.CSS_RESOURCE_PATH;
+import static vilij.settings.PropertyTypes.GUI_RESOURCE_PATH;
+import static vilij.settings.PropertyTypes.ICONS_RESOURCE_PATH;
+
+/**
+ * This is the application's user interface implementation.
+ *
+ * @author Ritwik Banerjee
+ */
+public final class AppUI extends UITemplate {
+
+    /**
+     * The application to which this class of actions belongs.
+     */
+    ApplicationTemplate applicationTemplate;
+
+    @SuppressWarnings("FieldCanBeLocal")
+    private Button scrnshotButton; // toolbar button to take a screenshot of the data
+    private LineChart<Number, Number> chart;          // the chart where data will be displayed
+    private Button displayButton;  // workspace button to display data on the chart
+    private TextArea textArea;       // text area for new data input
+    private boolean hasNewText;     // whether or not the text area has any new data since last display
+    private CheckBox checkBox;
+
+    public void setHasNewText(boolean hasNewText) {
+        this.hasNewText = hasNewText;
+    }
+
+    public LineChart<Number, Number> getChart() {
+        return chart;
+    }
+
+    public AppUI(Stage primaryStage, ApplicationTemplate applicationTemplate) {
+        super(primaryStage, applicationTemplate);
+        this.applicationTemplate = applicationTemplate;
+    }
+
+    @Override
+    protected void setResourcePaths(ApplicationTemplate applicationTemplate) {
+        super.setResourcePaths(applicationTemplate);
+        PropertyManager manager = applicationTemplate.manager;
+        cssPath = String.join("/",
+                manager.getPropertyValue(GUI_RESOURCE_PATH.name()),
+                manager.getPropertyValue(CSS_RESOURCE_PATH.name()),
+                manager.getPropertyValue(CSS_RESOURCE_FILENAME.name()));
+    }
+
+    @Override
+    protected void setToolBar(ApplicationTemplate applicationTemplate) {
+        super.setToolBar(applicationTemplate);
+        PropertyManager manager = applicationTemplate.manager;
+        String iconsPath = "/" + String.join(separator,
+                manager.getPropertyValue(GUI_RESOURCE_PATH.name()),
+                manager.getPropertyValue(ICONS_RESOURCE_PATH.name()));
+        String scrnshoticonPath = String.join(separator,
+                iconsPath,
+                manager.getPropertyValue(AppPropertyTypes.SCREENSHOT_ICON.name()));
+        scrnshotButton = setToolbarButton(scrnshoticonPath,
+                manager.getPropertyValue(AppPropertyTypes.SCREENSHOT_TOOLTIP.name()),
+                true);
+        toolBar.getItems().add(scrnshotButton);
+    }
+
+    @Override
+    protected void setToolbarHandlers(ApplicationTemplate applicationTemplate) {
+        applicationTemplate.setActionComponent(new AppActions(applicationTemplate));
+        newButton.setOnAction(e -> applicationTemplate.getActionComponent().handleNewRequest());
+        saveButton.setOnAction(e -> applicationTemplate.getActionComponent().handleSaveRequest());
+        loadButton.setOnAction(e -> applicationTemplate.getActionComponent().handleLoadRequest());
+        exitButton.setOnAction(e -> applicationTemplate.getActionComponent().handleExitRequest());
+        printButton.setOnAction(e -> applicationTemplate.getActionComponent().handlePrintRequest());
+        scrnshotButton.setOnAction(e -> ((AppActions) applicationTemplate.getActionComponent()).handleScreenshotRequest());
+    }
+
+    @Override
+    public void initialize() {
+        layout();
+        setWorkspaceActions();
+    }
+
+    @Override
+    public void clear() {
+        textArea.clear();
+        chart.getData().clear();
+    }
+
+    public TextArea getTextArea() {
+        return textArea;
+    }
+
+    private void layout() {
+        PropertyManager manager = applicationTemplate.manager;
+        NumberAxis xAxis = new NumberAxis();
+        NumberAxis yAxis = new NumberAxis();
+        chart = new LineChart<>(xAxis, yAxis);
+        chart.setTitle(manager.getPropertyValue(AppPropertyTypes.CHART_TITLE.name()));
+
+        VBox leftPanel = new VBox(8);
+        leftPanel.setAlignment(Pos.TOP_CENTER);
+        leftPanel.setPadding(new Insets(10));
+
+        VBox.setVgrow(leftPanel, Priority.ALWAYS);
+        leftPanel.setMaxSize(windowWidth * 0.29, windowHeight * 0.3);
+        leftPanel.setMinSize(windowWidth * 0.29, windowHeight * 0.3);
+
+        Text leftPanelTitle = new Text(manager.getPropertyValue(AppPropertyTypes.LEFT_PANE_TITLE.name()));
+        String fontname = manager.getPropertyValue(AppPropertyTypes.LEFT_PANE_TITLEFONT.name());
+        Double fontsize = Double.parseDouble(manager.getPropertyValue(AppPropertyTypes.LEFT_PANE_TITLESIZE.name()));
+        leftPanelTitle.setFont(Font.font(fontname, fontsize));
+
+        textArea = new TextArea();
+
+        HBox processButtonsBox = new HBox();
+        displayButton = new Button(manager.getPropertyValue(AppPropertyTypes.DISPLAY_BUTTON_TEXT.name()));
+        checkBox = new CheckBox(manager.getPropertyValue(AppPropertyTypes.CHECKBOX_LABEL.name()));
+        HBox.setHgrow(processButtonsBox, Priority.ALWAYS);
+        processButtonsBox.getChildren().addAll(displayButton, checkBox);
+
+        leftPanel.getChildren().addAll(leftPanelTitle, textArea, processButtonsBox);
+
+        StackPane rightPanel = new StackPane(chart);
+        rightPanel.setMaxSize(windowWidth * 0.69, windowHeight * 0.69);
+        rightPanel.setMinSize(windowWidth * 0.69, windowHeight * 0.69);
+        StackPane.setAlignment(rightPanel, Pos.CENTER);
+
+        workspace = new HBox(leftPanel, rightPanel);
+        HBox.setHgrow(workspace, Priority.ALWAYS);
+
+        appPane.getChildren().add(workspace);
+        VBox.setVgrow(appPane, Priority.ALWAYS);
+
+        applicationTemplate.getUIComponent().getPrimaryScene().getStylesheets().add(cssPath);
+    }
+
+    private void setWorkspaceActions() {
+        setTextAreaActions();
+        setDisplayButtonActions();
+        setCheckBoxAction();
+    }
+
+    //ActionLister for textArea. When the user releases the key this action is triggered.
+    // It checks if there is any text in the application then the new and save button's are enabled.
+    // When the text is empty then the save and new buttons are disabled.
+    private void setTextAreaActions() {
+
+        textArea.textProperty().addListener((observable, oldValue, newValue) -> {
+
+            if (!newValue.equals(oldValue) && !newValue.isEmpty()) {
+                hasNewText = true;
+                newButton.setDisable(false);
+                saveButton.setDisable(false);
+                ((AppActions) applicationTemplate.getActionComponent()).setIsUnsavedProperty(true);
+
+            } else {
+                hasNewText = false;
+                newButton.setDisable(true);
+                saveButton.setDisable(true);
+                ((AppActions) applicationTemplate.getActionComponent()).setIsUnsavedProperty(false);
+            }
+
+        });
+    }
+
+    private void setDisplayButtonActions() {
+        displayButton.setOnAction(event -> {
+            if (hasNewText) {
+                try {
+                    chart.getData().clear();
+                    AppData dataComponent = (AppData) applicationTemplate.getDataComponent();
+                    dataComponent.clear();
+                    dataComponent.loadData(textArea.getText());
+                    dataComponent.displayData();
+
+                    if (chart.getData().isEmpty()) {
+                        scrnshotButton.setDisable(true);
+                    } else {
+                        scrnshotButton.setDisable(false);
+                    }
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+            }
+            hasNewText = false;
+        });
+    }
+
+    private void setCheckBoxAction() {
+        checkBox.setOnAction(event -> {
+            if (checkBox.isSelected()) {
+                textArea.setEditable(false);
+                textArea.setStyle("-fx-control-inner-background: #D3D3D3");
+            } else {
+                textArea.setEditable(true);
+                textArea.setStyle(null);
+            }
+        });
+    }
+
+    public Button getSaveButton() {
+        return saveButton;
+    }
+
+    public Button getNewButton() {
+        return newButton;
+    }
+    
+   
+}
